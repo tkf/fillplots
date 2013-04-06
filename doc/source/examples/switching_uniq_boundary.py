065b19c9f3fdde73a1cb4271f04e3494de3a9470
Takafumi Arakaki <aka.tkf@gmail.com>
2013-04-06 22:20:48 +0200

    Setting config for boundary should work

--- doc/source/examples/switching_uniq_boundary.py
+++ doc/source/examples/switching_uniq_boundary.py
@@ -3,6 +3,9 @@ sqrt = boundary(lambda x: x ** 0.5)
 one = boundary(1)
 two = boundary(2)
 
+one.config.line_args = {'color': 'k', 'linestyle': 'dashed'}
+two.config.line_args = {'color': 'k', 'linestyle': 'dotted'}
+
 clg = Coloring([
     [(sqrt, True), (one,), (two, True)],
     [(sqrt,), (lambda x: x, True), (two, True)],
