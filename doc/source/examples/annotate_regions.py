79fd9ea0bc6cb06b172664680e9d587db0d0bcdc
Takafumi Arakaki <aka.tkf@gmail.com>
2013-04-07 13:05:58 +0200

    Remove FIXME from annotate_regions.py

--- doc/source/examples/annotate_regions.py
+++ doc/source/examples/annotate_regions.py
@@ -1,9 +1,9 @@
 from ineqfill import Coloring, annotate_regions
 clg = Coloring([
     [(lambda x: x ** 2,),
-     (lambda x: x * 0.5 + 5,)],  # FIXME: make it work w/o  * 0.5
+     (lambda x: x + 5,)],
     [(lambda x: - x ** 2, True),
-     (lambda x: x * 0.5 - 5, True)],  # FIXME: make it work w/o  * 0.5
+     (lambda x: x - 5, True)],
 ])
 clg.plot()
 annotate_regions(clg.regions, 'Annotation')
