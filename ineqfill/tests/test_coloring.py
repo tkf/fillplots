78f4f844ad0eae2690d130e7721517753659b506
Takafumi Arakaki <aka.tkf@gmail.com>
2013-04-07 03:27:39 +0200

    Add test_switching_uniq_boundary

--- ineqfill/tests/test_coloring.py
+++ ineqfill/tests/test_coloring.py
@@ -1,4 +1,11 @@
+import operator
+
 from ..api import Coloring
+from ..boundaries import boundary
+
+
+def mapcall(name, iterative):
+    return map(operator.methodcaller(name), iterative)
 
 
 def test_simple():
@@ -27,7 +34,29 @@ def test_config_inheritance():
 
     ax = clg.config.ax
     lines = ax.get_lines()
-    colors = [l.get_color() for l in lines]
-    widths = [l.get_linewidth() for l in lines]
+    colors = mapcall('get_color', lines)
+    widths = mapcall('get_linewidth', lines)
     assert colors == ['blue'] + ['black'] * 3
     assert widths == [5] + [1.0] * 3
+
+
+def test_switching_uniq_boundary():
+    sqrt = boundary(lambda x: x ** 0.5)
+    one = boundary(1)
+    two = boundary(2)
+
+    one.config.line_args = {'color': 'k', 'linestyle': 'dashed'}
+    two.config.line_args = {'color': 'k', 'linestyle': 'dotted'}
+
+    clg = Coloring([
+        [(sqrt, True), (one,), (two, True)],
+        [(sqrt,), (lambda x: x, True), (two, True)],
+        [(lambda x: x * (4 - x) / 2, True),
+         (two,)],
+    ], xlim=(0, 4), ylim=(0, 4))
+    clg.plot()
+
+    ax = clg.config.ax
+    lines = ax.get_lines()
+    colors = mapcall('get_color', lines)
+    assert colors == ['b', 'k', 'k', 'g', 'r']
