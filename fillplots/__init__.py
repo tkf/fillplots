418aa07822a8a82b32a78df98a4d3031556bbd00
Takafumi Arakaki <aka.tkf@gmail.com>
2013-04-08 23:47:15 +0200

    Update fillplots/__init__.py

--- fillplots/__init__.py
+++ fillplots/__init__.py
@@ -3,20 +3,42 @@
 fillplots -- Library to plot regions and boundaries given inequalities
 ======================================================================
 
-Links:
+.. sidebar:: Links:
 
-* `Documentation <http://fillplots.readthedocs.org/>`_ (at Read the Docs)
-* `Repository <https://github.com/tkf/fillplots>`_ (at GitHub)
-* `Issue tracker <https://github.com/tkf/fillplots/issues>`_ (at GitHub)
-* `PyPI <http://pypi.python.org/pypi/fillplots>`_
-* `Travis CI <https://travis-ci.org/#!/tkf/fillplots>`_ |build-status|
+   * `Documentation <http://fillplots.readthedocs.org/>`_ (at Read the Docs)
 
+     - `Examples <http://fillplots.readthedocs.org/en/latest/examples.html>`_
+
+   * `Repository <https://github.com/tkf/fillplots>`_ (at GitHub)
+   * `Issue tracker <https://github.com/tkf/fillplots/issues>`_ (at GitHub)
+   * `PyPI <http://pypi.python.org/pypi/fillplots>`_
+   * `Travis CI <https://travis-ci.org/#!/tkf/fillplots>`_ |build-status|
+
+``fillplots`` is a library to plot regions and boundaries given
+systems of inequality.  Here is a simple example to fill region like
+a piece of pie.
+
+>>> from fillplots import plot_regions
+>>> plot_regions([
+...     [(lambda x: (1.0 - x ** 2) ** 0.5,),
+...      (lambda x: 0.5 * x,),
+...      (lambda x: 2.0 * x, True)]
+... ], xlim=(0, 1), ylim=(0, 1))
+<fillplots.api.Plotter object at 0x...>
+
+See documentation_ and examples_ for more information.
 
 License
 -------
 
 `fillplots` is licensed under the terms of the BSD 2-Clause License.
 See the COPYING for more information.
+
+
+.. |build-status|
+   image:: https://secure.travis-ci.org/tkf/fillplots.png?branch=master
+   :target: http://travis-ci.org/tkf/fillplots
+   :alt: Build Status
 
 """
 # [[[end]]]
