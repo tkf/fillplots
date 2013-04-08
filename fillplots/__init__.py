0647db39149a3de369c213202971fc2dc3ec2270
Takafumi Arakaki <aka.tkf@gmail.com>
2013-04-08 17:36:50 +0200

    Add __doc__ etc. to fillplots/__init__.py

--- fillplots/__init__.py
+++ fillplots/__init__.py
@@ -1,3 +1,30 @@
+# [[[cog import cog; cog.outl('"""\n%s\n"""' % file('../README.rst').read())]]]
+"""
+fillplots -- Library to plot regions and boundaries given inequalities
+======================================================================
+
+Links:
+
+* `Documentation <http://fillplots.readthedocs.org/>`_ (at Read the Docs)
+* `Repository <https://github.com/tkf/fillplots>`_ (at GitHub)
+* `Issue tracker <https://github.com/tkf/fillplots/issues>`_ (at GitHub)
+* `PyPI <http://pypi.python.org/pypi/fillplots>`_
+* `Travis CI <https://travis-ci.org/#!/tkf/fillplots>`_ |build-status|
+
+
+License
+-------
+
+`fillplots` is licensed under the terms of the BSD 2-Clause License.
+See the COPYING for more information.
+
+"""
+# [[[end]]]
+
 from .api import *
 from .boundaries import boundary
 from .regions import annotate_regions
+
+__version__ = '0.0.1.dev0'
+__author__ = 'Takafumi Arakaki'
+__license__ = 'BSD License'
