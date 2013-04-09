9973c3e4c2b9303b5382c93f6cda50de59d7f7f0
Takafumi Arakaki <aka.tkf@gmail.com>
2013-04-09 13:18:37 +0200

    Simplify examples/explicit_regions.py
    Thanks to the previous commit.

--- doc/source/examples/explicit_regions.py
+++ doc/source/examples/explicit_regions.py
@@ -2,12 +2,10 @@ from fillplots import plot_regions, And, Or
 dom01 = (0, 1)
 dom12 = (1, 2)
 plotter = plot_regions([
-    And([(1, True),
-         (lambda x: (1.0 - x ** 2) ** 0.5, True, dom01),
+    And([(lambda x: (1.0 - x ** 2) ** 0.5, True, dom01),
          (lambda x: 0.5 * x, False, dom01),
          (lambda x: 2.0 * x, True, dom01)]),
-    Or([(1,),
-        (lambda x: (1.0 - (x - 1) ** 2) ** 0.5, True, dom12),
+    Or([(lambda x: (1.0 - (x - 1) ** 2) ** 0.5, True, dom12),
         (lambda x: 0.5 * (x - 1), False, dom12),
         (lambda x: 2.0 * (x - 1), True, dom12)]),
 ], xlim=(0, 2), ylim=(0, 1))
