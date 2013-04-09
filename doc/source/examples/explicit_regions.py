f711ee1aea491bba3b707d971a96ed8475c9c1e6
Takafumi Arakaki <aka.tkf@gmail.com>
2013-04-09 14:12:26 +0200

    Fix indent

--- doc/source/examples/explicit_regions.py
+++ doc/source/examples/explicit_regions.py
@@ -6,6 +6,6 @@ plotter = plot_regions([
          (lambda x: 0.5 * x, False, dom01),
          (lambda x: 2.0 * x, True, dom01)]),
     SDOr([(lambda x: (1.0 - (x - 1) ** 2) ** 0.5, True, dom12),
-        (lambda x: 0.5 * (x - 1), False, dom12),
-        (lambda x: 2.0 * (x - 1), True, dom12)]),
+          (lambda x: 0.5 * (x - 1), False, dom12),
+          (lambda x: 2.0 * (x - 1), True, dom12)]),
 ], xlim=(0, 2), ylim=(0, 1))
